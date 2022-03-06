a9adc32e3f68b642c5e8eb68fec8f7b81ab37af4
nikoagge <nikos@souniolabs.com>
2022-03-06 20:35:23 +0200

    Initial Commit

---
+++ WhatsAppClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by Nikos Aggelidis on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
